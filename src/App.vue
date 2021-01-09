1381033c2c5380554211f8a14fd06e699e16b153
tugce-7 <tugce.evci7@gmail.com>
2021-01-09 20:57:26 +0300

    movie-app-v1

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+<header>
+        <!-- Header Start -->
+        <div class="header-area header-transparent">
+            <div class="main-header header-sticky">
+                <div class="container-fluid">
+                    <div class="menu-wrapper d-flex align-items-center justify-content-between">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="">
+                            <img src="assets/img/logo/logo.png" alt=""> 
+                              </a>
+                        </div>
+                        <!-- Main-menu -->
+                        <div class="main-menu f-right d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a href="">ANASAYFA</a></li>
+                                    <li><a href="">FAVORİLER</a></li>
+                                    <li><a href="">KATEGORİLER</a></li>                                   
+
+                                    <!--DROPDOWN ACILIR MENU                                      
+                                      
+                                       <li><a href="#">Pages</a>
+                                        <ul class="submenu">
+                                            <li><a href="">listing Details</a></li>
+                                            <li><a href="">Catagories</a></li> 
+                                        </ul>
+                                    </li>
+                                    <li><a href="">Blog</a>
+                                        <ul class="submenu">
+                                            <li><a href="">Blog</a></li>
+                                            <li><a href="">Blog Details</a></li>
+                                            <li><a href="">Elements</a></li>
+                                        </ul>
+                                    </li> -->
+                                 
+                                </ul>
+                            </nav>
+                        </div>          
+                        <!-- Header-btn -->
+                        <div class="header-btns d-none d-lg-block f-right">
+                            <a href="#" class="mr-40"><i class="ti-user"></i> Giriş Yap</a>
+                            <a href="#" class="btn">Liste Ekle</a>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+ 
+        <!--? Hero Area Start-->
+        <div class="slider-area hero-bg1 hero-overly">
+            <div class="single-slider hero-overly  slider-height1 d-flex align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-10 col-lg-10">
+                            <!-- Hero Caption -->
+                            <div class="hero__caption pt-100">
+                                <h1> FİLM BOX </h1>
+                                <p>En güncel film listesine bizimle ulaşın!</p>
+                            </div>
+                            <!--Hero form -->
+                            <form action="#" class="search-box mb-100">
+                                <div class="input-form">
+                                    <input type="text" placeholder="Aramak istediğiniz filmin adını giriniz.">
+                                </div>
+                           
+                                <div class="search-form">
+                                    <a href="#"><i class="ti-search"></i> Ara</a>
+                                </div>	
+                            </form>	
+                            <!-- hero category1 img -->
+                          
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
