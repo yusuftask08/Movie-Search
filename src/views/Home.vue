c3b7e669bd9f69975f77118208d67be5895f9b33
tugce-7 <tugce.evci7@gmail.com>
2021-01-11 20:48:37 +0300

    cm

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   
    <search />
+   <cards/>
   
 </template>
 
 <script>
 // @ is an alias to /src
 import search from '@/components/search';
+import cards from '@/components/cards';
 
 export default {
   name: 'Home',
   components: {
-    search,
+    search,cards
   }
 }
 </script>
