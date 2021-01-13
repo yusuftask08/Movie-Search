d67cb7c86522018d0509d2c77206413d58baf7b9
burcineren <burcin.ern969@gmail.com>
2021-01-13 23:44:39 +0300

    V-1.0.0

---
+++ src/components/appHeader.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <header>
+    <div class="header-area ">
+      <div class="main-header header-sticky">
+        <div class="">
+          <div class="menu-wrapper d-flex align-items-center justify-content-between">
+               <nav class="navbar-expand-lg navbar navbarColor" >
+                <a class="navbar-brand collapse navbar-collapse" href="#">Film Box</a>
+                <router-link to="/" class="mr-5">Anasayfa</router-link>
+                <router-link to="/favorites">Favorilerim</router-link>
+              </nav>
+            <!-- Header-btn -->
+            <div class="header-btns d-none d-lg-block f-right ">
+              <a href="#" class="btn mr-5">Giriş</a>
+              <a href="#" class="btn">Kayıt Ol</a>
+            </div>
+            <!-- Mobile Menu -->
+            <div class="col-12">
+              <div class="mobile_menu d-block d-lg-none"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Header End -->
+  </header>
+</div>
+</template>
+<style scoped>
+
+</style>
--- src/components/appSearchBar.vue
+++ src/components/appSearchBar.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    
     <div class="slider-area hero-bg1 hero-overly">
       <div
         class="single-slider hero-overly slider-height1 d-flex align-items-center"
       >
+      
         <div class="container">
+          
           <div class="row justify-content-center">
             <div class="col-xl-10 col-lg-10">
               <!-- Hero Caption -->
-              <div class="hero__caption pt-100">
-                <h1>FİLM BOX</h1>
+              <div class="hero__caption pt-100 ">
+                <h1 class="">FİLM BOX</h1>
                 <p>En güncel film listesine bizimle ulaşın!</p>
               </div>
               <!--Hero form -->
@@ -17,12 +20,12 @@
                 <div class="input-form">
                   <input
                     type="text"
-                    placeholder="Aramak istediğiniz filmin adını giriniz."
+                    placeholder="Aramak istediğiniz filmin adını giriniz." @keydown.enter="searchMovie($event.target.value)"
                   />
                 </div>
 
-                <div class="search-form">
-                  <a href="#"><i class="ti-search"></i> Ara</a>
+                <div class="search-form header-btns">
+                  <a href="#" class="btn"><i class="ti-search"></i> Ara</a>
                 </div>
               </form>
               <!-- hero category1 img -->
@@ -33,3 +36,11 @@
     </div>
   </div>
 </template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    ...mapActions(["searchMovie"]),
+  },
+};
+</script>
