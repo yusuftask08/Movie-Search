a13e6177b8812f3db26d786a9995fffb313d32af
burcineren <burcin.ern969@gmail.com>
2021-01-14 16:15:52 +0300

    Version 1.0.1

--- src/components/appCardsItem.vue
+++ src/components/appCardsItem.vue
@@ -1,38 +1,39 @@
 <template>
-    <div  class="container">
-        <div class="card-media">
-    <!-- media container -->
-    <div class="card-media-object-container">
-      
-      <img :src="moviePoster" alt="">
-      <span class="card-media-object-tag subtle">{{ movie.Year }}</span>
-    </div>
-    <!-- body container -->
-    <div class="card-media-body">
-      <div class="card-media-body-top">
-        <!-- <div class="card-media-body-top-icons u-float-right">
-          <svg fill="#888888" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
-            <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-
-          </svg>
-        </div> -->
+    <!-- <div  class="container">
+      <div class="card-media">
+        <div class="card-media-object-container">
+          <img :src="moviePoster" alt="">
+          <span class="card-media-object-tag subtle">{{ movie.Year }}</span>
+        </div>
+        <div class="card-media-body">
+          <span class="card-media-body-heading">{{ movie.Title }}</span>
+              <div class="card-media-body-supporting-bottom card-media-body-supporting-bottom-reveal">
+            <span class="card-media-body-supporting-bottom-text subtle">{{ movie.Type }}</span>
+            <button  @click="addToFavorites" :class="favoriteClasses">Favorilere Ekle</button>
+            <a target="_blank"
+              :href="`https://www.imdb.com/title/${movie.imdbID}/`"
+              class="button"
+              >IMDb</a >
+          </div>
+        </div>
       </div>
-      
-      <span class="card-media-body-heading">{{ movie.Title }}</span>
-          <div class="card-media-body-supporting-bottom card-media-body-supporting-bottom-reveal">
-        <span class="card-media-body-supporting-bottom-text subtle">{{ movie.Type }}</span>
-        <button  @click="addToFavorites" :class="favoriteClasses">Favorilere Ekle</button>
-        <a target="_blank"
-          :href="`https://www.imdb.com/title/${movie.imdbID}/`"
-          class="button"
-          >IMDb</a >
-        <!-- <a href="#/" class="card-media-body-supporting-bottom-text card-media-link u-float-right"><i class="is_favourite fa fa-heart"></i>Favorilere Ekle</a> -->
+    </div> -->
+      <div class="wrapper">
+        <div class="product-img">
+          <img :src="moviePoster" height="420" width="327">
+        </div>
+        <div class="product-info">
+          <div class="product-text">
+            <h1>{{ movie.Title }}</h1>
+            <h2>{{ movie.Year }}</h2>
+            <p> {{ movie.Type }}<br> <i class="fas fa-thumbs-up"></i> </p>
+          </div>
+          <div class="product-price-btn">
+            <a :href="`https://www.imdb.com/title/${movie.imdbID}/`" target="_blank"><p><span></span>IMDB</p></a>
+            <button @click="addToFavorites" :class="favoriteClasses" type="button">Favorilerime Ekle</button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-    </div>
-    
 </template>
 <script>
 import { mapGetters } from "vuex";
@@ -63,3 +64,124 @@ export default {
   },
 };
 </script>
+<style scoped>
+body {
+  background-color: #fdf1ec;
+}
+
+.wrapper {
+  height: 420px;
+  width: 654px;
+  margin: 50px auto;
+  border-radius: 7px 7px 7px 7px;
+  /* VIA CSS MATIC https://goo.gl/cIbnS */
+  -webkit-box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);
+  -moz-box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);
+  box-shadow: 0px 14px 32px 0px rgba(0, 0, 0, 0.15);
+}
+
+.product-img {
+  float: left;
+  height: 420px;
+  width: 327px;
+}
+
+.product-img img {
+  border-radius: 7px 0 0 7px;
+}
+
+.product-info {
+  float: left;
+  height: 420px;
+  width: 327px;
+  border-radius: 0 7px 10px 7px;
+  background-color: #ffffff;
+}
+
+.product-text {
+  height: 300px;
+  width: 327px;
+}
+
+.product-text h1 {
+  margin: 0 0 0 38px;
+  padding-top: 52px;
+  font-size: 34px;
+  color: #474747;
+}
+
+.product-text h1,
+.product-price-btn p {
+  font-family: 'Bentham', serif;
+}
+
+.product-text h2 {
+  margin: 0 0 47px 38px;
+  font-size: 13px;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+  text-transform: uppercase;
+  color: #d2d2d2;
+  letter-spacing: 0.2em;
+}
+
+.product-text p {
+  height: 125px;
+  margin: 0 0 0 38px;
+  font-family: 'Playfair Display', serif;
+  color: #8d8d8d;
+  line-height: 1.7em;
+  font-size: 15px;
+  font-weight: lighter;
+  overflow: hidden;
+}
+
+.product-price-btn {
+  height: 103px;
+  width: 327px;
+  margin-top: 17px;
+  position: relative;
+}
+
+.product-price-btn p {
+  display: inline-block;
+  position: absolute;
+  top: -13px;
+  height: 50px;
+  font-family: 'Trocchi', serif;
+  margin: 0 0 0 38px;
+  font-size: 28px;
+  font-weight: lighter;
+  color: #474747;
+}
+
+span {
+  display: inline-block;
+  height: 50px;
+  font-family: 'Suranna', serif;
+  font-size: 34px;
+}
+.product-price-btn button {
+  float: right;
+  display: inline-block;
+  height: 50px;
+  width: 176px;
+  margin: 0 40px 0 16px;
+  box-sizing: border-box;
+  border: transparent;
+  border-radius: 60px;
+  font-family: 'Raleway', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  color: #ffffff;
+  background-color: #9cebd5;
+  cursor: pointer;
+  outline: none;
+}
+
+.product-price-btn button:hover {
+  background-color: #79b0a1;
+}
+</style>
